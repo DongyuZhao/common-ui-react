86686bc39b3ce6f048048f4ea7a89bfe69c1156d
Dongyu Zhao <dy.zhao@microsoft.com>
2018-05-23 01:19:31 +0800

    Add styles for basic html elements

--- src/App.tsx
+++ src/App.tsx
@@ -14,6 +14,36 @@ class App extends React.Component {
                 <p className='App-intro'>
                     To get started, edit <code>src/App.tsx</code> and save to reload.
                 </p>
+                <table className='table table-borderless table-dark'>
+                    <thead>
+                        <tr>
+                            <th scope='col'>#</th>
+                            <th scope='col'>First</th>
+                            <th scope='col'>Last</th>
+                            <th scope='col'>Handle</th>
+                        </tr>
+                    </thead>
+                    <tbody>
+                        <tr className='background-green'>
+                            <th scope='row'>1</th>
+                            <td>Mark</td>
+                            <td>Otto</td>
+                            <td>@mdo</td>
+                        </tr>
+                        <tr className='background-red'>
+                            <th scope='row'>2</th>
+                            <td>Jacob</td>
+                            <td>Thornton</td>
+                            <td>@fat</td>
+                        </tr>
+                        <tr className='background-yellow'>
+                            <th scope='row'>3</th>
+                            <td>Larry</td>
+                            <td>the Bird</td>
+                            <td>@twitter</td>
+                        </tr>
+                    </tbody>
+                </table>
             </div>
         );
     }
