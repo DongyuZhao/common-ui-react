46f0e76d64fc3f6b3336d8eb438d2f3ce66b564e
Dongyu Zhao <dy.zhao@outlook.com>
2018-05-28 15:40:23 +0800

    Add Button Component

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,9 @@
 import * as React from 'react';
 import './App.css';
 
+import { Button } from './components/input/Button';
 import logo from './logo.svg';
+import { Theme } from './shared/Theme';
 
 class App extends React.Component {
     public render() {
@@ -14,6 +16,20 @@ class App extends React.Component {
                 <p className='App-intro'>
                     To get started, edit <code>src/App.tsx</code> and save to reload.
                 </p>
+                <div className='container text-left'>
+                    <Button theme={Theme.Primary} outline={true} disabled={true} fill={true}>From Microsoft, built with love.</Button>
+                    <Button theme={Theme.Info} outline={true}>From Microsoft, built with love.</Button>
+                    <Button theme={Theme.Warning} outline={true}>From Microsoft, built with love.</Button>
+                    <Button theme={Theme.Caution} outline={true} fill={true}>From Microsoft, built with love.</Button>
+                    <Button theme={Theme.Error} outline={true}>From Microsoft, built with love.</Button>
+                    <Button theme={Theme.Success} outline={true}>From Microsoft, built with love.</Button>
+                    <Button theme={Theme.Primary} outline={false} disabled={true} fill={true}>From Microsoft, built with love.</Button>
+                    <Button theme={Theme.Info} outline={false}>From Microsoft, built with love.</Button>
+                    <Button theme={Theme.Warning} outline={false}>From Microsoft, built with love.</Button>
+                    <Button theme={Theme.Caution} outline={false} fill={true}>From Microsoft, built with love.</Button>
+                    <Button theme={Theme.Error} outline={false}>From Microsoft, built with love.</Button>
+                    <Button theme={Theme.Success} outline={false}>From Microsoft, built with love.</Button>
+                </div>
             </div>
         );
     }
